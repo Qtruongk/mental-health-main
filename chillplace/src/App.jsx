2ae117d6b55b77836c2911911287edb75dcbd1de
Qtruongk <qtruong2109.itech@gmail.com>
2023-08-16 19:30:05 +0700

    update source

--- chillplace/src/App.jsx
+++ chillplace/src/App.jsx
@@ -42,9 +42,33 @@ function App() {
         <Route path="/blog/caffeine-addiction" element={<CaffeineAddiction />} />
         <Route path="/blog/clinical-psychology" element={<ClinicalPsy />} />
         <Route path="/blog/cognitive-behavioral-therapy" element={<CognitiveBehavioral />} />
+        <Route path="/blog/drug-addiction" element={<DrugAddiction />} />
+        <Route path="/blog/depression-childhood" element={< DepressionChildhood/>} />
+        <Route path="/blog/delayed-sleep-phase-syndrome" element={<DelayedSleepSyndrome/>} />
+        <Route path="/blog/eating-disorders" element={<EatingDisorder />} />
+        <Route path="/blog/genZ-&-peer-pressure" element={<PeerPressure />} />
+        <Route path="/blog/gender-dysphoria" element={<GenderDysphoria />} />
+        <Route path="/blog/grief-loss" element={<Griefloss />} />
+        <Route path="/blog/intellectual-disability" element={<Intellectual />} />
+        <Route path="/blog/loneliness" element={<Loneliness />} />
+        <Route path="/blog/mixed-personality-disorder" element={<MixedPersonality />} />
+        <Route path="/blog/mood-disorders" element={<MoodDisorder />} />
+        <Route path="/blog/night-eating-syndrome" element={<NightEating />} />
+        <Route path="/blog/non-24-hour-sleep-wake-disorder" element={<SleepWake/>} />
+        <Route path="/blog/obessive-compulsive-disorder" element={<ObessiveCompulsive />} />
+        <Route path="/blog/online-therapy" element={<OnlineTherapy />} />
+        <Route path="/blog/panic-disorder" element={<PanicDisorder />} />
+        <Route path="/blog/paraphilic-disorders" element={<ParaphilicDisorder />} />
+        <Route path="/blog/quit-smoking" element={<QuitSmoking />} />
+        <Route path="/blog/sex-addiction" element={<SexAddiction />} />
+        <Route path="/blog/sleep-disorders" element={<SleepDisorder />} />
+        <Route path="/blog/shopping-addiction" element={<ShoppingAddiction />} />
+        <Route path="/blog/social-anxiety-disorder" element={<SocialAnxietty />} />
+        <Route path="/blog/technology-addiction" element={<TechnologyAddiction />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
